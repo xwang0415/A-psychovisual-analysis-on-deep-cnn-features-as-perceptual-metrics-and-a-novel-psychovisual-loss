d1123bbbbfd89ad4fc96cb7d5743e537cb4c082a
taimoor6864 <45258250+taimoor6864@users.noreply.github.com>
2018-12-07 00:26:52 +0900

    Add files via upload

--- main.m
+++ main.m
@@ -1,14 +1,9 @@
-gt=imread('C:\Users\KAIST\Desktop\MEE\Summer2018_Research\databaserelease2\refimgs\plane.bmp');
-im_ff=imread('C:\Users\KAIST\Desktop\MEE\Summer2018_Research\databaserelease2\fastfading\img58.bmp');
-im_jpg=imread('C:\Users\KAIST\Desktop\MEE\Summer2018_Research\databaserelease2\jpeg\img165.bmp');
-im_ckb=imread('C:\Users\KAIST\Desktop\4xUNET4_7l11ssim2vggrelu3_1\UNET_output015.png');
-im_gt=imread('C:\Users\KAIST\Desktop\22.png');
-im1=imnoise(gt,'salt & pepper',0.1);
-[act1,act2,loss,pksnr] = vgg_loss(gt,gt);
-size_act1 = size(act1);
-size_act2 = size(act2);
-act1 = reshape(act1,[size_act1(1) size_act1(2) size_act1(4)]);
-act2 = reshape(act2,[size_act2(1) size_act2(2) size_act2(4)]);
-tsne_plot(act1,act2);
-
-
+net = vgg16; %you can select a pretrained network avaliable on MATLAB(VGG-16, VGG-19 etc) or train your own network.
+layer = 'relu3_2';  %you need to select a layer. The list of layers with their dimensions is provided in net.Layers.
+fmams = 256; %the number of feature maps in the selected layer, provided in net.Layers.
+[VFSS,weights,act_mat] = weight_generator2(net,layer,fmaps);
+%The function returns the VFSS of each feature map, normalized VFSS -
+%weights and spatial frequency vs mean activation curves.
+%The weights can be saved in a .mat file and used for loss functions in
+%pytorch, tensorflow etc. 
+act_plotter(act_mat,act_mat,act_mat); %plotting activations vs spatial frequency.
